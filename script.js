d5fdbc00b5d70c953b75f12ebdcebc8abc04ad3c
Manish Elaganti <65448066+Manishreddy619@users.noreply.github.com>
2021-07-01 16:28:53 +0200

    using only new numbers without repition

--- script.js
+++ script.js
@@ -4,21 +4,51 @@ const numbercells = () => {
 	for (let i = 1; i < 77; i++) {
 		let newcell = document.createElement('td');
 		newcell.innerText = i;
-		newcell.classList.add('numbercell');
+		newcell.id = 'numbercell';
 		cellsContainer.appendChild(newcell);
 	}
 };
+window.onload = function () {
+	numbercells();
+};
 
-numbercells();
 const generateNumber = () => {
 	let random = Math.floor(Math.random() * 76) + 1;
-	// console.log(random);
+
+	console.log(`generated random number 🤓:${random}`);
 	let value = document.querySelectorAll('td');
 
 	for (let i = 0; i < value.length; i++) {
-		if (parseInt(value[i].innerText) === random) {
-			value[i].style.color = 'green';
-			value[i].style.backgroundColor = 'orange';
+		if (
+			value[i].style.color === 'green' &&
+			value[i].style.backgroundColor === 'orange'
+		) {
+			console.log(
+				'-----------------used 😛bingo numbers until now------------------',
+			);
+			console.log(` ${value[i].innerText}`);
+			console.log('-----------------------------------');
+		} else {
+			if (parseInt(value[i].innerText) === random) {
+				// console.log(random, 'random number');
+				// console.log(parseInt(value[i].innerText), 'bingo number');
+				console.log(
+					`generated random number: ${random} is matching😍😎 bingo number ${parseInt(
+						value[i].innerText,
+					)}`,
+				);
+				value[i].style.color = 'green';
+				value[i].style.backgroundColor = 'orange';
+			}
 		}
 	}
 };
+// let randomNumbers = [];
+// const letsrun = () => {
+// 	for (let i = 0; i < 77; i++) {
+// 		let random = Math.floor(Math.random() * 76) + 1;
+// 		randomNumbers.push(random);
+// 	}
+// 	return randomNumbers;
+// };
+// console.log(letsrun());
